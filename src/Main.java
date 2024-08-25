29c8d5bcfcf6672339b920ce141c2ad7a433701f
YUrkij <urok21887@gmail.com>
2024-08-25 14:50:46 +0500

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("Hello and welcome!");
+
+        for (int j = 1; j <= 5; j++) {
+            System.out.println("j = " + j);
+        }
+    }
+}
